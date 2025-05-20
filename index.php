43edbd239d2c04ba53a16dc4ecb7ebbddf6f654a
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-05-20 12:47:11 -0400

    added header and footer

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <html>
     <head>
+        <title>Contact Manager - Home</title>
+        <link rel="stylesheet" type="txt/css" href="main.css"/>
+    
 </head>
     <body>
+        <?php include('header.php'); ?>
+
+        <main>
+            <h2>Contact List</h2>
+        </main>
+
+        <?php include('footer.php'); ?>
+        
+            
 </body>
 </html>

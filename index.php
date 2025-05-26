d64b30f60ae3df52063cfba625aedbc5c29c7e6b
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-05-26 11:48:52 -0400

    updated index.php

--- index.php
+++ index.php
@@ -1,18 +1,51 @@
+
+<?php
+    session_start();
+    require("database.php");
+    $queryContacts = 'SELECT * FROM contacts';
+    $statement1 = $db->prepare($queryContacts);
+    $statement1->execute();
+    $contacts = $statement1->fetchAll();
+
+    $statement1->closeCursor();
+?>
+<!DOCTYPE html>
 <html>
     <head>
         <title>Contact Manager - Home</title>
-        <link rel="stylesheet" type="txt/css" href="main.css"/>
-    
-</head>
+        <link rel="stylesheet" type="txt/css" href="css/main.css" />
+    </head>
     <body>
-        <?php include('header.php'); ?>
+        <?php include("header.php"); ?>
 
         <main>
             <h2>Contact List</h2>
+
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                    <th>Phone Number</th>
+                    <th>Status</th>
+                    <th>Birth Date</th>
+                </tr>
+
+                <?php foreach ($contacts as $contact): ?>
+                    <tr>
+                        <td><?php echo $contact['firstName']; ?></td>
+                        <td><?php echo $contact['lastName']; ?></td>
+                        <td><?php echo $contact['emailAddress']; ?></td>
+                        <td><?php echo $contact['phone']; ?></td>
+                        <td><?php echo $contact['status']; ?></td>
+                        <td><?php echo $contact['dob']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+            </table>
+            <p><a href="add_contact_form.php">Add Contact</a></p>
         </main>
 
-        <?php include('footer.php'); ?>
-        
-            
-</body>
+        <?php include("footer.php"); ?>
+    </body>
 </html>
